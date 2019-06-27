4c164b88f53c7f62b82c8673a107c13a0750d87f
Abner Ribeiro <abnergoulart@yahoo.com.br>
2019-06-26 21:23:47 -0300

    treinando projeto com jquery

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('button').click(function(){
+		$('.navbar-brand').html(`
+		<img src="${dados.avatar_url}" class="avatar d-inline-block align-top">
+		${dados.name}
+		`);
+		$(".navbar-brand"). addClass("d-flex align-items-center");
+		$('nav').toggleClass('trocar-cor bg-danger');
+	});
+});
